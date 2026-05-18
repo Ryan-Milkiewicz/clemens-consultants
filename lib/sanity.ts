9306551242b641a1c579ec93b7790a2b0585b0de
RyanMilkiewicz <ryanmilkiewicz@gmail.com>
2026-05-18 09:35:31 -0400

    use cdn

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -6,7 +6,7 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   apiVersion: "2026-03-01",
-  useCdn: false,
+  useCdn: true,
 });
 
 const builder = createImageUrlBuilder(client);
