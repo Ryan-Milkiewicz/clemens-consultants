9995aaecd3c3f2d1fd61b32988df23aae98652ea
RyanMilkiewicz <ryanmilkiewicz@gmail.com>
2026-05-19 12:42:24 -0400

    added metadata tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,27 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Clemens Consultants",
+export const metadata = {
+  title: "Clemens Consultants - Expert Accounting & Financial Services",
+  description:
+    "Clemens Consultants provides expert accounting and financial services for businesses in the Capital District",
+  openGraph: {
+    title: "Clemens Consultants - Expert Accounting & Financial Services",
+    description:
+      "Clemens Consultants provides expert accounting and financial services for businesses in the Capital District",
+    url: "https://clemensconsultants.com",
+    siteName: "Clemens Consultants",
+    type: "website",
+  },
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -23,8 +42,40 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const jsonLd = {
+    "@context": "https://www.schema.org",
+    "@type": ["AccountingService", "ProfessionalService"],
+    name: "Clemens Consultants",
+    url: "https://www.clemensconsultants.com/",
+    description:
+      "Clemens Consultants provides expert accounting and financial services for businesses in the Capital District",
+    address: {
+      "@type": "PostalAddress",
+      streetAddress: "54 First Dyke Road, Unit 2",
+      addressLocality: "Averill Park",
+      addressRegion: "NY",
+      postalCode: "12018",
+      addressCountry: "US",
+    },
+    contactPoint: {
+      "@type": "ContactPoint",
+      telephone: "+1-518-960-9227",
+      contactType: "Customer Service",
+      areaServed: "US",
+      availableLanguage: "English",
+    },
+  };
   return (
     <html lang="en">
+      <head>
+        <title>
+          Clemens Consultants - Expert Accounting & Financial Services
+        </title>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
