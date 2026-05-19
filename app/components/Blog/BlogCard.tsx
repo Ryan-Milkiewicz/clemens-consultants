40142afbd24d6a9449aa2638c4a50cf7d13c32c0
RyanMilkiewicz <ryanmilkiewicz@gmail.com>
2026-05-19 12:58:25 -0400

    fix text and date to center

--- app/components/Blog/BlogCard.tsx
+++ app/components/Blog/BlogCard.tsx
@@ -19,19 +19,21 @@ export default function BlogCard({ post }: { post: BlogPost }) {
           />
         </div>
       )}
-      <div className="p-4 flex-1">
-        <h3 className="text-xl font-semibold text-[#1e3557] mb-2">
-          {post.title}
-        </h3>
-        <p className="text-gray-600 mb-4">{post.excerpt}</p>
+      <div className="p-4 flex-1 flex flex-col justify-between">
+        <div>
+          <h3 className="text-xl font-semibold text-[#1e3557] mb-2">
+            {post.title}
+          </h3>
+          <p className="text-gray-600 mb-4">{post.excerpt}</p>
+        </div>
         <Link
           href={`/blog/${post.slug}`}
-          className="text-[#47a4a4] font-semibold hover:underline"
+          className="text-[#47a4a4] font-semibold hover:underline text-center"
         >
           Read More
         </Link>
       </div>
-      <div className="p-3 border-t border-gray-300 bg-gray-100 mt-auto">
+      <div className="p-3 border-t border-gray-300 bg-gray-100 text-center">
         <p className="text-sm text-gray-500">
           <DateComponent dateString={post.date.toString()} />
         </p>
